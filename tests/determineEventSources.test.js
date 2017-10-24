7d31fc78614619eba9911084f3cce1b340acca98
Adrian Castro <basehat@gmail.com>
2017-10-24 16:40:37 -0700

    eslint fixes

--- tests/determineEventSources.test.js
+++ tests/determineEventSources.test.js
@@ -4,12 +4,12 @@ var fs = require("fs");
 import determineEventSources from '../src/aws/lambda/determineEventSource';
 
 test('Event Type Detection Tests', async (t) => {
-  await glob('./tests/aws/**/*.json', false, function (er, files) {
-    for(let file in files) {
+  await glob('./tests/aws/**/*.json', false, (er, files) => {
+    for (const file in files) {
       const contents = fs.readFileSync(files[file]);
       const eventObject = JSON.parse(contents);
       const eventType = determineEventSources(eventObject);
-      t.comment("Found content type: " + JSON.stringify(eventType));
+      t.comment(`Found content type: ${JSON.stringify(eventType)}`);
     }
   });
   t.end();
