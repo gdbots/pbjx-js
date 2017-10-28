1bae70924a374b27d99167809775ecfce5caf74c
Greg Brown <greg@12dnetworks.com>
2017-10-27 19:03:03 -0700

    aws-mock is indeed not working.  doh!

--- tests/aws/lambda/getConfig.test.js
+++ tests/aws/lambda/getConfig.test.js
@@ -1,8 +1,14 @@
 import test from 'tape';
 import aws from 'aws-sdk-mock';
+import realAws from 'aws-sdk';
+import getConfig from '../../../src/aws/lambda/getConfig';
 
-test('getConfig tests', (t) => {
-  aws.mock('SSM', 'getParameters', (arams, callback) => {
+aws.setSDKInstance(realAws);
+
+test('getConfig tests', async (t) => {
+  aws.mock('SSM', 'getParameters', (params, callback) => {
+    t.comment('mock worked');
+
     callback(null, {
       InvalidParameters: [],
       Parameters: [
@@ -19,6 +25,19 @@ test('getConfig tests', (t) => {
       ],
     });
   });
+
+  try {
+    const config = await getConfig({
+      pbjxHost: '',
+      pbjxReceiveKey: '',
+    });
+
+    t.same(config.pbjxHost, 'test1');
+    t.same(config.pbjxReceiveKey, 'test2');
+  } catch (e) {
+    t.fail('getConfig failed');
+  }
+
   aws.restore();
 
   t.end();
