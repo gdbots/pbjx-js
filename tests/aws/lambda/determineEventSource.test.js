72218e276f688833e47d2047857ce5172a92243e
Adrian Castro <basehat@gmail.com>
2017-10-27 19:20:40 -0700

    alpha sort event source names and registrations

--- tests/aws/lambda/determineEventSource.test.js
+++ tests/aws/lambda/determineEventSource.test.js
@@ -2,14 +2,14 @@ import test from 'tape';
 
 import determineEventSource, { eventSources } from '../../../src/aws/lambda/determineEventSource';
 
-import mockEventCloudfront from './sample-events/cloudfront.json';
 import mockEventApiGateway from './sample-events/apigateway-proxy.json';
+import mockEventCloudfront from './sample-events/cloudfront.json';
 import mockEventCognitoSync from './sample-events/cognito-sync.json';
 import mockEventDynamoDB from './sample-events/dynamodb.json';
 import mockEventIotButton from './sample-events/iot-button.json';
-import mockEventLex from './sample-events/lex.json';
 import mockEventKinesis from './sample-events/kinesis.json';
 import mockEventKinesisFirehose from './sample-events/kinesis-firehose.json';
+import mockEventLex from './sample-events/lex.json';
 import mockEventS3 from './sample-events/s3.json';
 import mockEventScheduled from './sample-events/scheduled-event.json';
 import mockEventSes from './sample-events/ses.json';
@@ -18,18 +18,18 @@ import mockEventSns from './sample-events/sns.json';
 const eventTypeMatches = (event, expected) => determineEventSource(event) === expected;
 
 test('Validate function detection via determineEventSource tests', async (t) => {
-  t.ok(eventTypeMatches(mockEventCloudfront, eventSources.CLOUDFRONT));
   t.ok(eventTypeMatches(mockEventApiGateway, eventSources.APIGATEWAY_PROXY));
+  t.ok(eventTypeMatches(mockEventCloudfront, eventSources.CLOUDFRONT));
   t.ok(eventTypeMatches(mockEventCognitoSync, eventSources.COGNITO_SYNC));
   t.ok(eventTypeMatches(mockEventDynamoDB, eventSources.DYNAMODB));
+  t.ok(eventTypeMatches(mockEventIotButton, eventSources.IOT_BUTTON));
   t.ok(eventTypeMatches(mockEventKinesis, eventSources.KINESIS));
   t.ok(eventTypeMatches(mockEventKinesisFirehose, eventSources.KINESIS_FIREHOSE));
+  t.ok(eventTypeMatches(mockEventLex, eventSources.LEX));
   t.ok(eventTypeMatches(mockEventS3, eventSources.S3));
   t.ok(eventTypeMatches(mockEventScheduled, eventSources.SCHEDULED));
   t.ok(eventTypeMatches(mockEventSes, eventSources.SES));
   t.ok(eventTypeMatches(mockEventSns, eventSources.SNS));
-  t.ok(eventTypeMatches(mockEventLex, eventSources.LEX));
-  t.ok(eventTypeMatches(mockEventIotButton, eventSources.IOT_BUTTON));
   t.end();
 });
 
